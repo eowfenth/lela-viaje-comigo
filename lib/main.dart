76c899c50ce0b54b38ff30c4ff315244e7e3437d
Nícolas 'eowf' Deçordi <eowfenth.services@gmail.com>
2024-01-08 15:34:52 -0300

    feat: set initial routes for fun

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/igo',
+      routes: {
+        '/igo': (context) => const MyHomePage(title: 'Flutter Demo Home Page 2'),
+        '/nico': (context) => const MyHomePage(title: 'Flutter Demo Home Page 2'),
+        '/overview': (context) => const MyHomePage(title: 'Flutter Demo Home Page 3'),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +37,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
